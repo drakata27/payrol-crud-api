d45a270e30b9fac4a11a2a2e2f8a0cbc1e5afe99
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-11-16 20:46:09 +0000

    Upgraded API structure

--- src/main/java/com/example/payroll/Employee.java
+++ src/main/java/com/example/payroll/Employee.java
@@ -8,32 +8,32 @@ import java.util.Objects;
 
 @Entity
 public class Employee {
-    private @Id
-    @GeneratedValue Long id;
-    private String name;
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee() {}
 
-    public Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
-
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+    public void setName(String name) {
+        String [] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getRole() {
@@ -44,22 +44,124 @@ public class Employee {
         this.role = role;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employee)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof Employee))
+            return false;
         Employee employee = (Employee) o;
-        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-                && Objects.equals(this.role, employee.role);
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+                && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+                + '\'' + ", role='" + this.role + '\'' + '}';
     }
 }
+
+//@Entity
+//class Employee {
+//
+//    private @Id @GeneratedValue Long id;
+//    private String firstName;
+//    private String lastName;
+//    private String role;
+//
+//    public Employee() {}
+//
+//    public Employee(String firstName, String lastName, String role) {
+//
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.role = role;
+//    }
+//
+//    public String getName() {
+//        return this.firstName + " " + this.lastName;
+//    }
+//
+//    public void setName(String name) {
+//        String[] parts = name.split(" ");
+//        this.firstName = parts[0];
+//        this.lastName = parts[1];
+//    }
+//
+//    public Long getId() {
+//        return this.id;
+//    }
+//
+//    public String getFirstName() {
+//        return this.firstName;
+//    }
+//
+//    public String getLastName() {
+//        return this.lastName;
+//    }
+//
+//    public String getRole() {
+//        return this.role;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//
+//        if (this == o)
+//            return true;
+//        if (!(o instanceof Employee))
+//            return false;
+//        Employee employee = (Employee) o;
+//        return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+//                && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+//                + '\'' + ", role='" + this.role + '\'' + '}';
+//    }
+//}
--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.example.payroll;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -11,18 +13,42 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 public class EmployeeController {
     private final EmployeeRepository employeeRepository;
+    private final EmployeeModelAssembler employeeModelAssembler;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
+    public EmployeeController(EmployeeRepository employeeRepository, EmployeeModelAssembler employeeModelAssembler) {
         this.employeeRepository = employeeRepository;
+        this.employeeModelAssembler = employeeModelAssembler;
     }
 
     //GET
-    // TODO: Getting an aggregate root
+//    @GetMapping("/employees")
+//    List<Employee> all() {
+//        return employeeRepository.findAll();
+//    }
+    // GET
+//    @GetMapping("/employees")
+//    CollectionModel<EntityModel<Employee>> all() {
+//
+//        List<EntityModel<Employee>> employees = employeeRepository.findAll().stream()
+//                .map(employee -> EntityModel.of(employee,
+//                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+//                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+//                .collect(Collectors.toList());
+//
+//        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+//    }
+    // GET
     @GetMapping("/employees")
-    List<Employee> all() {
-        return employeeRepository.findAll();
-    }
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = employeeRepository
+                .findAll()
+                .stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
 
+        return  CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
+    // TODO: Proper Responses
     //POST
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
@@ -36,15 +62,23 @@ public class EmployeeController {
 //                .findById(id)
 //                .orElseThrow(() -> new EmployeeNotFoundException(id));
 //    }
+    // GET
+//    @GetMapping("/employees/{id}")
+//    EntityModel<Employee> one(@PathVariable Long id) {
+//
+//        Employee employee = employeeRepository.findById(id) //
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//
+//        return EntityModel.of(employee, //
+//                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+//                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+//    }
+    //GET
     @GetMapping("/employees/{id}")
     EntityModel<Employee> one(@PathVariable Long id) {
-
-        Employee employee = employeeRepository.findById(id) //
+        Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
-
-        return EntityModel.of(employee, //
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return employeeModelAssembler.toModel(employee);
     }
 
     //PUT
