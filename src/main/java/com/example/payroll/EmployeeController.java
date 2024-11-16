264d41fe43eb671b38ce7fdad44c971ab0fb7a2e
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-11-16 17:49:35 +0000

    first commit

---
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.example.payroll;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    //GET
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return employeeRepository.findAll();
+    }
+
+    //POST
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return employeeRepository.save(newEmployee);
+    }
+
+    //GET
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//        return employeeRepository
+//                .findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+    @GetMapping("/employees/{id}")
+    EntityModel<Employee> one(@PathVariable Long id) {
+
+        Employee employee = employeeRepository.findById(id) //
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+
+    //PUT
+    @PutMapping("/employees/{id}")
+    Employee updateEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return employeeRepository.save(employee);
+                })
+                .orElseGet(() -> employeeRepository.save(newEmployee));
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
